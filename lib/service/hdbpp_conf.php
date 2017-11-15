4bc9cf986225a941c0da079505d9f3d88b0f13de
Lucio Zambon <lucio.zambon@gmail.com>
2017-11-15 14:13:23 +0100

    Update hdbpp_conf.php
    add initialization for alarms

--- lib/service/hdbpp_conf.php
+++ lib/service/hdbpp_conf.php
@@ -19,4 +19,9 @@
   // if you wont to hide a part of your domain edit the following array
   $skipdomain = array('tango://srv-tango-srf.fcs.elettra.trieste.it:20000/'=>'');
 
+  define("ALARM_HOST", "host");
+  define("ALARM_USER", "username");
+  define("ALARM_PASSWORD", "password");
+  define("ALARM_DB", "db");
+
 ?>
