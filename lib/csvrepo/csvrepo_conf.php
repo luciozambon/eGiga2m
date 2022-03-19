06f0fe9f9fadf9bb3041e246ad7e7e0c790df768
Lucio Zambon <lucio.zambon@gmail.com>
2022-03-19 03:22:16 +0100

    Create csvrepo_conf.php

---
+++ lib/csvrepo/csvrepo_conf.php
@@ -0,0 +1,12 @@
+<?php
+
+define("HOST", "pghost");
+define("USERNAME", "pgusername");
+define("PASSWORD", "pgpassword");
+
+define("THRESHOLD_5M", 2*86400);
+define("THRESHOLD_1H", 24*86400);
+define("THRESHOLD_1D", 500*86400);
+
+$databases = array('pgdb0','pgdb1');
+?>
