309298ab19b4dac728d78edaf14e8b7f828be81a
Lucio Zambon <lucio.zambon@gmail.com>
2022-06-29 11:38:28 +0200

    Create hdbtimescale_conf.php

---
+++ lib/service/hdbtimescale_conf.php
@@ -0,0 +1,12 @@
+<?php
+define("HOST", "hostname");
+define("PORT", 5432);
+define("USERNAME", "username");
+define("PASSWORD", "secret-password");
+define("DBNAME", "hdb");
+$databases = array('hdb');
+$skipdomain = array(
+	  'tango://some-domain:20000/'=>'[shortcut] '
+  );
+
+?>
